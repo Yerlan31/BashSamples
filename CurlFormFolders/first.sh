6fc4a4cf867fe43b8850294170a1ca2886207e8d
Муханов Ержан <emukhanov@phoenixit.ru>
2022-03-17 00:42:27 +0300

    v1

--- CurlFormFolders/first.sh
+++ CurlFormFolders/first.sh
@@ -4,6 +4,11 @@ set -eu
 ################Создание имени файла##############
 FileName=`echo "${0}" | cut -d'.' -f2 | cut -d'/' -f2 `
 echo Hello world >  ${FileName}_$(date '+%Y%m%d_%H%M').txt
-##################################################
 
-
+#####Отправка курлов в цикле
+input="/c/sites.txt"
+while IFS= read -r line
+do
+  echo "$line"
+  curl "$line"
+done < "$input"
