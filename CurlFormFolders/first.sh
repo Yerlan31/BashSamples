e96b2e6e371e176deb2438da66eb8048ed8f0335
Муханов Ержан <emukhanov@phoenixit.ru>
2022-03-17 12:55:37 +0300

    finalclose

--- CurlFormFolders/first.sh
+++ CurlFormFolders/first.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-set -eu
+#/set -eu
 
 ################Создание имени файла##############
 FileName=`echo "${0}" | cut -d'.' -f2 | cut -d'/' -f2 `
-echo Hello world >  ${FileName}_$(date '+%Y%m%d_%H%M').txt
+#echo Hello world >  ${FileName}_$(date '+%Y%m%d_%H%M')
 
-#####Отправка курлов в цикле
+#####Отправка курлов в цикле / заменитьна параметр
 input="/c/sites.txt"
-while IFS= read -r line
+
+while read -r -e line
 do
-  echo "$line"
-  curl "$line"
+var=$(echo $line)
+echo $var
+curl -v -i "$var" >> ${FileName}_$(date '+%Y%m%d_%H%M')
 done < "$input"
